562ce608301b851282e4b3c370b3b6992e5a1c01
Adrián Vizi <adrian.vizi04@gmail.com>
2023-10-24 08:37:21 +0200

    Add project files.

---
+++ solys/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solys
+{
+    internal class BolygoC
+    {
+        public string Bolygó {get; set;}
+        public int HoldSzam { get; set;}
+        public int BolygTerfog { get; set;}
+    }
+}
