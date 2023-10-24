580f20b2a9804bb1a89c218bdf02557e2547722c
Adrián Vizi <adrian.vizi04@gmail.com>
2023-10-24 12:18:36 +0200

    a

--- solys/Class1.cs
+++ solys/Class1.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace solys
 {
-    internal class BolygoC
+    internal class Adat
     {
-        public string Bolygó {get; set;}
+        public string Bolygo {get; set;}
         public int HoldSzam { get; set;}
-        public int BolygTerfog { get; set;}
+        public double BolygTerfog { get; set;}
+
+        public Adat(string sor)
+        {
+            var m = sor.Split(';');
+            Bolygo = m[0];
+            HoldSzam = Convert.ToInt32(m[1]);
+            if (m[2].Contains('.')) BolygTerfog = double.Parse(m[2].Replace('.', ','));
+        }
+
     }
 }
--- solys/Program.cs
+++ solys/Program.cs
@@ -1,2 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using solys;
+using System.Numerics;
+
+List<Adat> adatok = new();
+using StreamReader sr = new(
+    path: @"..\..\..\SRC\solsys.txt",
+    encoding: System.Text.Encoding.UTF8);
+while (!sr.EndOfStream) adatok.Add(new Adat(sr.ReadLine()));
+
+Console.WriteLine(adatok.Count());
+
+Console.WriteLine("1.");
+Console.WriteLine($"\t{adatok.Count} db bolygü van a naprendszerben\n");
+
+Console.WriteLine("2.");
+var f2 = adatok.Average(x => x.HoldSzam);
+Console.WriteLine($"\tA naprendszer egy bolgyójának átlegosan {f2} db holdja van\n");
+
+Console.WriteLine("3.");
+var f3 = adatok.OrderByDescending(p => p.BolygTerfog)
+                         .First();
+Console.WriteLine($"\tA legnagyobb bolygó a {f3.Bolygo}");
+
+Console.Write("4. Írd be a keresett bolygó nevét: ");
+string bn = Console.ReadLine();
+bool f4 = false;
+foreach (Adat adat in adatok)
+{
+    if (adat.Bolygo == bn) f4 = !f4;
+}
+if (f4) Console.WriteLine("\tMegtalálható a listában a keresett bolygó");
+else Console.WriteLine("\tNem található meg a listában a keresett bolgyó");
+
+Console.Write("5. Írj be egy egész számot: ");
+int esz = Convert.ToInt32(Console.ReadLine());
+bool f5 = false;
+foreach (Adat adat in adatok)
+{
+    if (adat.HoldSzam > esz) f5 = !f5;
+}
+if (f5) Console.WriteLine($"\tA következő bolygóknak van {esz}-nél/nál több holdja:");
+else Console.WriteLine("Nincsen a keresettnek megfelelő bolygó");
+Console.ReadKey(); 
